b143ebf146e4979ce1280d535ec97c044bbcb690
Justin Bagaoisan <eugeneherbertthethird@Justins-MacBook-Air.local>
2024-09-20 15:34:38 -0700

    initialize stables

---
+++ stables.js
@@ -0,0 +1,9 @@
+let horseName="Esteban";
+console.log(horseName);
+horseName = "Mateusz";
+console.log(horseName);
+
+let horseAge = 300;
+console.log(horseAge);
+let isHorseInside = true;
+console.log(isHorseInside);
