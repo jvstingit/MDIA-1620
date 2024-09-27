00eca2ba2aa47baa36265b13db19ed848a15c1d9
Justin <jbagaoisan2@my.bcit.ca>
2024-09-27 16:21:22 -0700

    adding lab 4 work

--- stables.js
+++ stables.js
@@ -1,7 +1,7 @@
 // Lesson 3
-let horseName= "Esteban";
+let horseName = "Esteban";
 console.log(horseName);
-horseName = "Mateusz";
+let horseNickname = "Mateusz";
 console.log(horseName);
 
 let horseAge = 300;
@@ -14,8 +14,13 @@ console.log(typeof true)
 console.log(typeof false)
 const STABLE_MONTHLY_FEE = 5000;
 console.log(STABLE_MONTHLY_FEE);
-
+console.log(`MEOW ${STABLE_MONTHLY_FEE}`);
 console.log(93.74/32);
-console.log("cunt" + "slay" + "brat");
-
+console.log("woof" + "grrr" + "glub");
 console.log("Esteban" + 64);
+
+//LAB LESSON 4
+console.log(`mi horses nombre es ${horseName}`);
+console.log(`mi horses nick nombre es ${horseNickname}`);
+console.log("THE MONTHLY STABLE FEE IS" + STABLE_MONTHLY_FEE);
+console.log("the stables fee with a discounted 3 month stay would be " + (STABLE_MONTHLY_FEE * 3) *.1 )
