ffd6f0f15ed6d0509d250b15f94b2ce9d6ba8e9b
jvstingit <eugeneherbertthethird@Justins-MacBook-Air.local>
2024-09-27 15:21:27 -0700

    adding info to stables

--- stables.js
+++ stables.js
@@ -1,4 +1,5 @@
-let horseName="Esteban";
+// Lesson 3
+let horseName= "Esteban";
 console.log(horseName);
 horseName = "Mateusz";
 console.log(horseName);
@@ -7,3 +8,14 @@ let horseAge = 300;
 console.log(horseAge);
 let isHorseInside = true;
 console.log(isHorseInside);
+
+//Lesson 4
+console.log(typeof true)
+console.log(typeof false)
+const STABLE_MONTHLY_FEE = 5000;
+console.log(STABLE_MONTHLY_FEE);
+
+console.log(93.74/32);
+console.log("cunt" + "slay" + "brat");
+
+console.log("Esteban" + 64);
