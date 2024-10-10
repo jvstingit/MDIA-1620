cabc282b734a4267da38f4de819100093aff7dc3
Justin <jbagaoisan2@my.bcit.ca>
2024-10-09 18:40:24 -0700

    lab 5 changes

--- stables.js
+++ stables.js
@@ -24,3 +24,8 @@ console.log(`mi horses nombre es ${horseName}`);
 console.log(`mi horses nick nombre es ${horseNickname}`);
 console.log("THE MONTHLY STABLE FEE IS" + STABLE_MONTHLY_FEE);
 console.log("the stables fee with a discounted 3 month stay would be " + (STABLE_MONTHLY_FEE * 3) *.1 )
+
+//LAB 5a
+if (horseName === "Esteban" && isHorseInside !== true){
+    console.log(`Mr. ${horseName} is inside the stable`);}
+        else{console.log(`Mr. ${horseName} is not outside`);}
